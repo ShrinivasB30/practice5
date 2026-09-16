ea3ae6ae12d2e9be7bea8cdc950b3f117353ee3d
Shrinivas <shri@gmail.com>
2026-09-16 21:59:34 +0530

    updated in feature

--- login.java
+++ login.java
@@ -2,5 +2,6 @@ class login
 {
     public static void main(String[] args) {
         System.out.println("Welcome to the login page!");
+        System.out.println("Please enter your username and password.");
     }
 }
