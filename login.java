f1d8eb3410d074a8aa5e5a0770b7365e2e0672ab
Shrinivas <shri@gmail.com>
2026-09-16 21:58:33 +0530

    Revert "updated in main"
    This reverts commit 388e65318dde73cec00f2ed5ddea8da3d9a5853c.

--- login.java
+++ login.java
@@ -2,6 +2,5 @@ class login
 {
     public static void main(String[] args) {
         System.out.println("Welcome to the login page!");
-        System.out.println("Please enter your username and password.");
     }
 }
